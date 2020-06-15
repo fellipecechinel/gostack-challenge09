6983e5368c3e9c3740fffe45d008559fb2797c73
Fellipe Cechinel <fellipecechinel@gmail.com>
2020-06-15 11:07:09 -0300

    Added code to services and controller to Orders

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -17,16 +17,81 @@ interface IRequest {
   products: IProduct[];
 }
 
+interface IProductInsert {
+  product_id: string;
+  price: number;
+  quantity: number;
+}
+
+interface IProductUpdate {
+  id: string;
+  quantity: number;
+}
+
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Invalid customer');
+    }
+
+    const productsIds = products.map(product => ({ id: product.id }));
+
+    const productsInDatabase = await this.productsRepository.findAllById(
+      productsIds,
+    );
+
+    if (productsInDatabase.length !== products.length) {
+      throw new AppError('There are invalid products');
+    }
+
+    const productsToInsert: IProductInsert[] = [];
+    const productsToUpdate: IProductUpdate[] = [];
+
+    products.forEach(product => {
+      const productDatabase = productsInDatabase.find(
+        item => item.id === product.id,
+      );
+
+      if (!productDatabase) return;
+
+      if (productDatabase?.quantity < product.quantity) {
+        throw new AppError(`Product ${product.id} has insufficient quantity`);
+      }
+
+      productsToUpdate.push({
+        id: product.id,
+        quantity: productDatabase.quantity - product.quantity,
+      });
+
+      productsToInsert.push({
+        product_id: product.id,
+        quantity: product.quantity,
+        price: productDatabase.price,
+      });
+    });
+
+    await this.productsRepository.updateQuantity(productsToUpdate);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: productsToInsert,
+    });
+
+    return order;
   }
 }
 
