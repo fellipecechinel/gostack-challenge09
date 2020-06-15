96d6dd1cca01f2e22f25e89e2061a21b395371d9
Fellipe Cechinel <fellipecechinel@gmail.com>
2020-06-15 11:04:20 -0300

    Chaged models Orders and OrdersProcucts to use typeorm

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -5,29 +5,53 @@ import {
   UpdateDateColumn,
   JoinColumn,
   PrimaryGeneratedColumn,
+  ValueTransformer,
   ManyToOne,
 } from 'typeorm';
 
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
+const columnNumericTransformer: ValueTransformer = {
+  from: (data?: string | null) => {
+    return data ? parseFloat(data) : null;
+  },
+  to: (data?: number | null) => {
+    return data || null;
+  },
+};
+
+@Entity('orders_products')
 class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id' })
   order: Order;
 
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: 'product_id' })
   product: Product;
 
+  @Column()
   product_id: string;
 
+  @Column()
   order_id: string;
 
+  @Column('numeric', {
+    transformer: columnNumericTransformer,
+  })
   price: number;
 
+  @Column('integer')
   quantity: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
