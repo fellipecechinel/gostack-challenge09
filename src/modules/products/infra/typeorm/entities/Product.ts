7c875d7c3bc7c0a4118e3bc4b4b4a56f6ddcf2b6
Fellipe Cechinel <fellipecechinel@gmail.com>
2020-06-15 07:59:15 -0300

    Added codes to create product

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -4,24 +4,45 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
-  OneToMany,
+  ValueTransformer,
+  // OneToMany,
 } from 'typeorm';
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+const columnNumericTransformer: ValueTransformer = {
+  from: (data?: string | null) => {
+    return data ? parseFloat(data) : null;
+  },
+  to: (data?: number | null) => {
+    return data || null;
+  },
+};
+
+@Entity('products')
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column('numeric', {
+    transformer: columnNumericTransformer,
+  })
   price: number;
 
+  @Column('numeric', {
+    transformer: columnNumericTransformer,
+  })
   quantity: number;
 
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,41 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
   }
 
-  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+  public async findAllById(productsIds: IFindProducts[]): Promise<Product[]> {
+    const products = await this.ormRepository.find({
+      where: {
+        id: In(productsIds),
+      },
+    });
+
+    return products;
   }
 
   public async updateQuantity(
-    products: IUpdateProductsQuantityDTO[],
+    _products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    return [];
   }
 }
 
